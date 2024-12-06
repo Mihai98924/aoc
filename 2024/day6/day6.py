3e7e1374e1ac1862063e7d98c52591a699f3f368
Mihai Pop <mihai@elnino.tech>
2024-12-06 23:58:31 +0100

    feat(day6-2024): add solution and input for part 2

--- 2024/day6/day6.py
+++ 2024/day6/day6.py
@@ -2,8 +2,6 @@
 di = [-1, 0, 1, 0]
 dj = [0, 1, 0, -1]
 
-queue = []
-
 def bounds(x, y, matrix):
     if x < 0 or x >= len(matrix):
         return False
@@ -11,33 +9,65 @@ def bounds(x, y, matrix):
         return False
     return True
 
-def lee(matrix, x, y):
+def lee(matrix, x, y, loops=False):
     i = 0
+    visited = []
+    obstacles = []
     while bounds(x,y,matrix):
-        if matrix[x][y] == '.' or matrix[x][y] == '^' or matrix[x][y] == 'X':
-            if bounds(x+di[i%len(di)],y+dj[i%len(dj)],matrix) and matrix[x+di[i%len(di)]][y+dj[i%len(dj)]] == '#':
-                i+=1
+        if matrix[x][y] in '.^X':
+            if loops:
+                if ([x+di[i%len(di)],y+dj[i%len(dj)]],[x,y]) in obstacles:
+                    return True
             t = list(matrix[x])
             t[y] = 'X'
             matrix[x]= ''.join(t)
-            if [x,y] not in queue:
-                queue.append([x,y])
+            if [x,y] not in visited:
+                visited.append([x,y])
+            while bounds(x+di[i%len(di)],y+dj[i%len(dj)],matrix) and matrix[x+di[i%len(di)]][y+dj[i%len(dj)]] in 'O#':
+                obstacles.append(([x + di[i % len(di)], y + dj[i % len(dj)]], [x, y]))
+                i+=1
             x += di[i%len(di)]
             y += dj[i%len(dj)]
-    for m in matrix:
-        print(m)
+    return visited
 
+def find_start(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == '^':
+                return i,j
 
-def part1(data):
-    for i in range(len(data)):
-        for j in range(len(data[i])):
-            if data[i][j] == '^':
-                lee(data, i, j)
-                break
-    print(len(queue))
+def part1(matrix):
+    data = matrix.copy()
+    i,j = find_start(data)
+    queue = lee(data, i, j)
+    return queue
+
+def clean(matrix):
+    for i in range(len(matrix)):
+        matrix[i].replace('X', '.')
+        matrix[i].replace('O', '.')
+    return matrix
+
+def part2(matrix, queue):
+    s = 0
+    i, j = find_start(matrix)
+    for q in queue:
+        if q == [i,j]:
+            continue
+        t = list(matrix[q[0]])
+        t[q[1]] = 'O'
+        matrix[q[0]] = ''.join(t)
+        if lee(matrix, i, j, True) == True:
+            s += 1
+        matrix = clean(matrix)
+    print(s)
+
 
 with open("input.txt") as f:
     matrix = f.readlines()
     matrix = [str(m.replace("\n", "")) for m in matrix]
-    part1(matrix)
+    queue = part1(matrix)
+    print(len(queue))
+    part2(matrix, queue)
+
 
