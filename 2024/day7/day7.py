c3ef5aef67a88a2fe56fb6a39e239697b2acfc6d
Mihai Pop <pop.mihai2410@gmail.com>
2024-12-07 12:24:28 +0100

    feat(day7-2024): add solution part 2

--- 2024/day7/day7.py
+++ 2024/day7/day7.py
@@ -1,14 +1,28 @@
 
-eqs = set()
+eqs = []
 
-def backtrack(eq, index, s, result):
+def backtrack(eq, index, s, result, concat=False):
     if index == len(eq):
-        if s == result:
-            eqs.add(tuple(eq))
+        if int(s) == result:
+            eqs.append(tuple(eq))
         return
     n = eq[index]
-    backtrack(eq, index + 1, s + n, result)
-    backtrack(eq, index + 1, s * n, result)
+    backtrack(eq, index + 1, int(s) + n, result, concat)
+    backtrack(eq, index + 1, int(s) * n, result, concat)
+    if concat:
+        backtrack(eq, index + 1, str(s) + str(n), result, concat)
+
+def part2(lines):
+    s = 0
+    for l in lines:
+        l[0] = int(l[0])
+        eq = l[1].split(" ")
+        l[1] = tuple([int(x) for x in eq])
+        backtrack(l[1], 1, l[1][0], l[0], concat=True)
+        if l[1] in eqs:
+            s += l[0]
+            print(l)
+    print(s)
 
 def part1(lines):
     s = 0
@@ -19,9 +33,10 @@ def part1(lines):
         backtrack(l[1], 1, l[1][0], l[0])
         if l[1] in eqs:
             s += l[0]
-            print(l[1])
+            print(l)
     print(s)
 
 with open('input.txt') as f:
     lines = [line.strip().split(": ") for line in f.readlines()]
-    part1(lines)
+    # part1(lines)
+    part2(lines)
