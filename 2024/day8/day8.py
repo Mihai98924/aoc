87ca8282344ac3bdfaec26ca43e2680435c42775
Mihai Pop <pop.mihai2410@gmail.com>
2024-12-09 15:46:49 +0100

    feat(day8-2024): add solution part 2

--- 2024/day8/day8.py
+++ 2024/day8/day8.py
@@ -6,6 +6,29 @@ def bounds(x, y, matrix):
         return False
     return True
 
+def part2(lines):
+    symbols = {}
+    for i in range(len(lines)):
+        for j in range(len(lines[i])):
+            if lines[i][j] not in ".":
+                if lines[i][j] not in symbols.keys():
+                    symbols[lines[i][j]] = [(i, j)]
+                else:
+                    symbols[lines[i][j]].append((i, j))
+    cnt = 0
+    for s in symbols.keys():
+        for i in range(len(symbols[s]) - 1):
+            for j in range(i + 1, len(symbols[s])):
+                for k in range(-len(lines), len(lines)):
+                    v =  (symbols[s][i][0] + k * (symbols[s][i][0] - symbols[s][j][0]), symbols[s][i][1] + k * (symbols[s][i][1] - symbols[s][j][1]))
+                    if bounds(v[0], v[1], lines) and lines[v[0]][v[1]] != "#":
+                        lines[v[0]] = lines[v[0]][:v[1]] + "#" + lines[v[0]][v[1] + 1:]
+    for i in range(len(lines)):
+        for j in range(len(lines[i])):
+            if lines[i][j] != ".":
+                cnt += 1
+    print(cnt)
+
 def part1(lines):
     symbols = {}
     for i in range(len(lines)):
@@ -28,9 +51,8 @@ def part1(lines):
                     cnt += 1
                     lines[v2[0]] = lines[v2[0]][:v2[1]] + "#" + lines[v2[0]][v2[1] + 1:]
     print(cnt)
-    for l in lines:
-        print(l)
 
 with open("input.txt") as f:
     lines = [line.strip() for line in f.readlines()]
-    part1(lines)
+    # part1(lines)
+    part2(lines)
