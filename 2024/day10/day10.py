3eebfc51f418a4ab348e00b4b4dc552393477738
Mihai Pop <pop.mihai2410@gmail.com>
2024-12-13 21:36:23 +0100

    feat(day10-2024): solution part 2

--- 2024/day10/day10.py
+++ 2024/day10/day10.py
@@ -11,7 +11,7 @@ def check_bounds(i, j, data):
 
 
 def get_trail(i, j, data, trail):
-    trail.add((i, j))
+    trail.append((i, j))
     if check_bounds(i + di[0], j + dj[0], data) and int(data[i + di[0]][j + dj[0]]) == int(data[i][j]) + 1:
         get_trail(i + di[0], j + dj[0], data, trail)
     if check_bounds(i + di[1], j + dj[1], data) and int(data[i + di[1]][j + dj[1]]) == int(data[i][j]) + 1:
@@ -21,6 +21,20 @@ def get_trail(i, j, data, trail):
     if check_bounds(i + di[3], j + dj[3], data) and int(data[i + di[3]][j + dj[3]]) == int(data[i][j]) + 1:
         get_trail(i + di[3], j + dj[3], data, trail)
 
+def part2(data):
+    starts = []
+    answer = 0
+    for i in range(len(data)):
+        trail = set()
+        for j in range(len(data[i])):
+            if data[i][j] == '0':
+                starts.append((i, j))
+    for s in starts:
+        trail = []
+        get_trail(s[0], s[1], data, trail)
+        results = [(i, j) for i, j in trail if data[i][j] == '9']
+        answer += int(len(results))
+    print(answer)
 
 def part1(data):
     starts = []
@@ -31,8 +45,9 @@ def part1(data):
             if data[i][j] == '0':
                 starts.append((i, j))
     for s in starts:
-        trail = set()
+        trail = []
         get_trail(s[0], s[1], data, trail)
+        trail = set(trail)
         results = [(i, j) for i, j in trail if data[i][j] == '9']
         answer += int(len(results))
     print(answer)
@@ -41,3 +56,4 @@ def part1(data):
 with open("input.txt") as f:
     lines = [line.strip() for line in f.readlines()]
     part1(lines)
+    part2(lines)
